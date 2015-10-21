7a7d186da88d1d0094bfb2048353e082c63881d4
Jenniffer Anzola <japtheb@gmail.com>
2015-10-21 15:02:02 -0500

    improve the heuristic function and sort first row and first column

--- src/Estado.java
+++ src/Estado.java
@@ -2,150 +2,199 @@ import java.util.*;
 
 public class Estado implements Comparable<Estado>, Comparator<Estado> {
 
-    public static Estado con(String entrada) {
-        String[] lineas = entrada.split("\\r?\\n");
-        int[][] matriz = new int[Puzzle.SIZE][Puzzle.SIZE];
-        for (int i = 0; i < lineas.length; i++) {
-            String[] datos = lineas[i].split("-");
-            for (int j = 0; j < datos.length; j++) {
-                matriz[i][j] = Integer.parseInt(datos[j]);
-            }
-        }
-        return new Estado(new Puzzle(matriz));
-    }
+	public static Estado con(String entrada) {
+		String[] lineas = entrada.split("\\r?\\n");
+		int[][] matriz = new int[Puzzle.SIZE][Puzzle.SIZE];
+		for (int i = 0; i < lineas.length; i++) {
+			String[] datos = lineas[i].split("-");
+			for (int j = 0; j < datos.length; j++) {
+				matriz[i][j] = Integer.parseInt(datos[j]);
+			}
+		}
+		return new Estado(new Puzzle(matriz));
+	}
 
-    public enum DIRECCION {
-        ARRIBA,
-        ABAJO,
-        DERECHA,
-        IZQUIERDA,
-    }
+	public enum DIRECCION {
+		ARRIBA, ABAJO, DERECHA, IZQUIERDA,
+	}
 
+	private final Puzzle puzzle;
+	private final int valor;
+	private boolean oredenadaL = false;
 
-    private final Puzzle puzzle;
-    private final int valor;
+	private Queue<DIRECCION> path = new LinkedList<DIRECCION>();
 
-    private Queue<DIRECCION> path = new LinkedList<>();
+	public Estado(Puzzle puzzle) {
+		this.puzzle = puzzle;
+		this.valor = calcluarValor();
+	}
 
-    public Estado(Puzzle puzzle) {
-        this.puzzle = puzzle;
-        this.valor = calcluarValor();
-    }
+	private int calcluarValor() {
+		int valor = 0;
+		for (int i = 0; i < puzzle.getPuzzle().length; i++) {
+			for (int j = 0; j < puzzle.getPuzzle()[i].length; j++) {
+				int valorCelda = puzzle.getPuzzle()[i][j] - 1;
 
-    private int calcluarValor() {
-        int valor = 0;
-        for (int i = 0; i < puzzle.getPuzzle().length; i++) {
-            for (int j = 0; j < puzzle.getPuzzle()[i].length; j++) {
-                int valorCelda = puzzle.getPuzzle()[i][j];
-                int valorEsperado = (i * Puzzle.SIZE) + j;
-                valor += Math.abs(valorCelda - valorEsperado);
-            }
-        }
-        return valor;
-    }
+				int penalizacion = 1;
+				if (puzzle.getPuzzle()[0][0] != 1) {
+					penalizacion = 15;
+				}
+				if (puzzle.getPuzzle()[0][1] != 2) {
+					penalizacion = 14;
+				}
+				if (puzzle.getPuzzle()[0][2] != 3) {
+					penalizacion = 13;
+				}
+				if (puzzle.getPuzzle()[0][3] != 4) {
+					penalizacion = 12;
+				}
+				if (puzzle.getPuzzle()[1][0] != 5) {
+					penalizacion = 11;
+				}
+				if (puzzle.getPuzzle()[2][0] != 9) {
+					penalizacion = 10;
+				}
+				if (puzzle.getPuzzle()[3][0] != 13) {
+					penalizacion = 9;
+				} 
+				
+				if(penalizacion==1){
+					oredenadaL = true;
+				}
 
-    public List<Estado> generarEstados() {
-        List<Estado> estados = new ArrayList<>();
-        Punto cero = this.puzzle.getZero();
+				int filaEsperada = (valorCelda / 4);
+				int columnaEsperada = valorCelda - (filaEsperada * Puzzle.SIZE);
 
-        if (cero.y - 1 >= 0)
-            estados.add(Estado.con(this, DIRECCION.DERECHA));
-        if (cero.x - 1 >= 0)
-            estados.add(Estado.con(this, DIRECCION.ABAJO));
-        if (cero.x + 1 < Puzzle.SIZE)
-            estados.add(Estado.con(this, DIRECCION.ARRIBA));
-        if (cero.y + 1 < Puzzle.SIZE)
-            estados.add(Estado.con(this, DIRECCION.IZQUIERDA));
+				valor += (Math.abs(filaEsperada - i) + Math.abs(columnaEsperada
+						- j))
+						* penalizacion;
+			}
+		}
+		return valor;
+	}
 
-        return estados;
-    }
+	public List<Estado> generarEstados() {
+		List<Estado> estados = new ArrayList<Estado>();
+		Punto cero = this.puzzle.getZero();
 
-    private static Estado con(Estado estado, DIRECCION direccion) {
-        int[][] puzzleCopia = new int[estado.getPuzzle().getPuzzle().length][];
-        for (int i = 0; i < estado.getPuzzle().getPuzzle().length; i++) {
-            puzzleCopia[i] = estado.getPuzzle().getPuzzle()[i].clone();
-        }
+		if (cero.y - 1 >= 0)
+			estados.add(Estado.con(this, DIRECCION.DERECHA));
+		if (cero.x - 1 >= 0)
+			estados.add(Estado.con(this, DIRECCION.ABAJO));
+		if (cero.x + 1 < Puzzle.SIZE)
+			estados.add(Estado.con(this, DIRECCION.ARRIBA));
+		if (cero.y + 1 < Puzzle.SIZE)
+			estados.add(Estado.con(this, DIRECCION.IZQUIERDA));
 
-        Punto cero = estado.getPuzzle().getZero();
+		return estados;
+	}
 
-        Punto puntoSwap;
-        switch (direccion) {
-            case DERECHA:
-                puntoSwap = new Punto(cero.x, cero.y - 1);
-                break;
-            case IZQUIERDA:
-                puntoSwap = new Punto(cero.x, cero.y + 1);
-                break;
-            case ABAJO:
-                puntoSwap = new Punto(cero.x - 1, cero.y);
-                break;
-            case ARRIBA:
-                puntoSwap = new Punto(cero.x + 1, cero.y);
-                break;
-            default:
-                throw new IllegalStateException("no es posible realizar este movimiento");
-        }
-        puzzleCopia[cero.x][cero.y] = puzzleCopia[puntoSwap.x][puntoSwap.y];
-        puzzleCopia[puntoSwap.x][puntoSwap.y] = 0;
-        Estado nuevoEstado = new Estado(new Puzzle(puzzleCopia));
-        nuevoEstado.addToPath(estado.getPath());
-        nuevoEstado.addToPath(direccion);
-        return nuevoEstado;
-    }
+	private static Estado con(Estado estado, DIRECCION direccion) {
+		int[][] puzzleCopia = new int[estado.getPuzzle().getPuzzle().length][];
+		for (int i = 0; i < estado.getPuzzle().getPuzzle().length; i++) {
+			puzzleCopia[i] = estado.getPuzzle().getPuzzle()[i].clone();
+		}
 
-    public Puzzle getPuzzle() {
-        return puzzle;
-    }
+		Punto cero = estado.getPuzzle().getZero();
 
-    public int getValor() {
-        return valor;
-    }
+		Punto puntoSwap;
+		switch (direccion) {
+		case DERECHA:
+			puntoSwap = new Punto(cero.x, cero.y - 1);
+			break;
+		case IZQUIERDA:
+			puntoSwap = new Punto(cero.x, cero.y + 1);
+			break;
+		case ABAJO:
+			puntoSwap = new Punto(cero.x - 1, cero.y);
+			break;
+		case ARRIBA:
+			puntoSwap = new Punto(cero.x + 1, cero.y);
+			break;
+		default:
+			throw new IllegalStateException(
+					"no es posible realizar este movimiento");
+		}
+		puzzleCopia[cero.x][cero.y] = puzzleCopia[puntoSwap.x][puntoSwap.y];
+		puzzleCopia[puntoSwap.x][puntoSwap.y] = 0;
+		Estado nuevoEstado = new Estado(new Puzzle(puzzleCopia));
+		nuevoEstado.addToPath(estado.getPath());
+		nuevoEstado.addToPath(direccion);
+		return nuevoEstado;
+	}
 
-    @Override public int compareTo(Estado o) {
-        return toString().compareTo(o.toString());
-    }
+	public Puzzle getPuzzle() {
+		return puzzle;
+	}
 
-    @Override public int compare(Estado o1, Estado o2) {
-        return o1.toString().compareTo(o2.toString());
-    }
+	public int getValor() {
+		return valor;
+	}
 
-    @Override public boolean equals(Object obj) {
-        if (obj instanceof Estado) {
-            int[][] puzzle = getPuzzle().getPuzzle();
-            int[][] puzzleComparador = ((Estado) obj).getPuzzle().getPuzzle();
-            for (int i = 0; i < puzzle.length; i++) {
-                for (int j = 0; j < puzzle[i].length; j++) {
-                    if (puzzle[i][j] != puzzleComparador[i][j]) {
-                        return false;
-                    }
-                }
-            }
-        } else {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public int compareTo(Estado o) {
+		return toString().compareTo(o.toString());
+	}
 
-    @Override public String toString() {
-        String resultado = "";
-        int[][] puzzle = getPuzzle().getPuzzle();
-        for (int i = 0; i < puzzle.length; i++) {
-            for (int j = 0; j < puzzle[i].length; j++) {
-                resultado += puzzle[i][j] + ",";
-            }
-        }
-        return resultado;
-    }
+	@Override
+	public int compare(Estado o1, Estado o2) {
+		return o1.toString().compareTo(o2.toString());
+	}
 
-    public Queue<DIRECCION> getPath() {
-        return path;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Estado) {
+			int[][] puzzle = getPuzzle().getPuzzle();
+			int[][] puzzleComparador = ((Estado) obj).getPuzzle().getPuzzle();
+			for (int i = 0; i < puzzle.length; i++) {
+				for (int j = 0; j < puzzle[i].length; j++) {
+					if (puzzle[i][j] != puzzleComparador[i][j]) {
+						return false;
+					}
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
+	}
 
-    public void addToPath(DIRECCION direccion) {
-        path.add(direccion);
-    }
+	@Override
+	public String toString() {
+		String resultado = "";
+		int[][] puzzle = getPuzzle().getPuzzle();
+		for (int i = 0; i < puzzle.length; i++) {
+			for (int j = 0; j < puzzle[i].length; j++) {
+				resultado += puzzle[i][j] + ",";
+			}
+		}
+		return resultado;
+	}
 
-    private void addToPath(Queue<DIRECCION> path) {
-        this.path.addAll(path);
-    }
+	public Queue<DIRECCION> getPath() {
+		return path;
+	}
+
+	public void addToPath(DIRECCION direccion) {
+		path.add(direccion);
+	}
+
+	private void addToPath(Queue<DIRECCION> path) {
+		this.path.addAll(path);
+	}
+	
+	public boolean getLOrdenada(){
+		return this.oredenadaL;
+	}
+
+	public void imprimir() {
+		int[][] puzzle = getPuzzle().getPuzzle();
+		for (int i = 0; i < puzzle.length; i++) {
+			String resultado = "";
+			for (int j = 0; j < puzzle[i].length; j++) {
+				resultado += puzzle[i][j] + ",";
+			}
+			System.out.println(resultado);
+		}
+	}
 }
--- src/Solucion.java
+++ src/Solucion.java
@@ -2,55 +2,61 @@ import java.util.*;
 
 public class Solucion {
 
-    public static int[][] objetivo =
-        new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
-    public static Estado estadoObjetivo = new Estado(new Puzzle(objetivo));
+	public static int[][] objetivo = new int[][] { { 1, 2, 3, 4 },
+			{ 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 0 } };
+	public static Estado estadoObjetivo = new Estado(new Puzzle(objetivo));
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int[][] datos = new int[][] {{11, 2, 4, 7}, {6, 0, 15, 8}, {10, 12, 3, 13}, {5, 9, 1, 14}};
+		int[][] datos = new int[][] { { 9, 12, 5, 4 }, { 13, 0, 1, 7 },
+				{ 2, 10, 6, 11 }, { 14, 8, 15, 3 } };
 
-        Estado inicial = new Estado(new Puzzle(datos));
-        solucionar(inicial);
-    }
+		Estado inicial = new Estado(new Puzzle(datos));
+		solucionar(inicial);
+	}
 
-    public static Estado solucionar(Estado inicial) {
-        List<Estado> abiertos = new ArrayList<>();
-        Set<Estado> cerrados = new TreeSet<>();
+	public static Estado solucionar(Estado inicial) {
+		List<Estado> abiertos = new ArrayList<Estado>();
+		Set<Estado> cerrados = new TreeSet<Estado>();
+		boolean ordenadaL = false;
 
-        abiertos.add(inicial);
+		abiertos.add(inicial);
 
-        while (abiertos.size() != 0) {
-            Estado estado = abiertos.get(0);
-            //System.out.println("estado actual " + estado.toString());
-            System.out.println(estado.getValor());
+		while (abiertos.size() != 0) {
+			Estado estado = abiertos.get(0);
 
-            if (estado.equals(estadoObjetivo)) {
-                System.out.println("hay solucion");
-                System.out.println(estado.getPath().toString());
-                return estado;
-            } else {
-                List<Estado> estados = estado.generarEstados();
-                //System.out.println("no hay solucion, generando " + estados.size() + " estados");
-                for (Estado estadoGenerado : estados) {
-                    if (!abiertos.contains(estadoGenerado) && !cerrados.contains(estadoGenerado)) {
-                        abiertos.add(estadoGenerado);
-                        //System.out.println("estado agregado " + estadoGenerado.toString());
-                    } else {
-                        //System.out.println("estado no agregado " + estadoGenerado.toString());
-                    }
-                }
-            }
-            cerrados.add(estado);
-            //System.out.println("estado cerrado " + estado.toString());
-            abiertos.remove(estado);
-            Collections.sort(abiertos, new Comparator<Estado>() {
-                @Override public int compare(Estado o1, Estado o2) {
-                    return ((Integer) o1.getValor()).compareTo(o2.getValor());
-                }
-            });
-            //System.out.println("aun existen " + abiertos.size() + " estados disponibles");
-        }
-        return null;
-    }
+			if (estado.equals(estadoObjetivo)) {
+				System.out.println("hay solucion");
+				estado.imprimir();
+				System.out.println("numero de movimientos actuales "
+						+ estado.getPath().size());
+				System.out.println(estado.getPath().toString());
+				return estado;
+			} else {
+
+				if (estado.getLOrdenada() && !ordenadaL) {
+					estado.imprimir();
+					System.out.println("numero de movimientos actuales "
+							+ estado.getPath().size());
+					ordenadaL = true;
+				}
+				List<Estado> estados = estado.generarEstados();
+				for (Estado estadoGenerado : estados) {
+					if (!abiertos.contains(estadoGenerado)
+							&& !cerrados.contains(estadoGenerado)) {
+						abiertos.add(estadoGenerado);
+					}
+				}
+			}
+			cerrados.add(estado);
+			abiertos.remove(estado);
+			Collections.sort(abiertos, new Comparator<Estado>() {
+				@Override
+				public int compare(Estado o1, Estado o2) {
+					return ((Integer) o1.getValor()).compareTo(o2.getValor());
+				}
+			});
+		}
+		return null;
+	}
 }
